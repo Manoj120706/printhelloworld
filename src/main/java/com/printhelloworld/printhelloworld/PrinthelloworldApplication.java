36b01165fe76401d5b3c6eb79ce648666f69ba2f
user <authornova615@gamil.com>
2021-11-27 10:33:26 +0530

    Initial commit

---
+++ src/main/java/com/printhelloworld/printhelloworld/PrinthelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.printhelloworld.printhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrinthelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrinthelloworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/printhelloworld/printhelloworld/PrinthelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.printhelloworld.printhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrinthelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
